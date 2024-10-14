e539337a7efa62555317e379cd73e4e885911816
bacisormiruna <mirunabacisor06@gmail.com>
2024-10-14 20:20:59 +0300

    Added classes for 'I' principle in solid

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,47 @@
 import bad.*;
-import good.*;
+import good.GoodInvoicePrinter;
+import good.GoodInvoicePrinter_I;
+import good.SomeOtherGoodInvoicePrinter;
+import good.SomeOtherGoodInvoicePrinter_I;
+import invoice.ComplexInvoice;
+import invoice.Invoice;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        testBadL();
-        testGoodL();
+        testBadI();
+        testGoodI();
     }
 
-    private static void testBadL() {
-        BadRectangle actuallySquare = new BadSquare(20, 30);
-        BadGraphicsService badGraphicsService = new BadGraphicsService();
-        badGraphicsService.checkForArea(actuallySquare);
+    private static void testBadI() {
+        Invoice invoice = new Invoice(-132523);
+        ComplexInvoice complexInvoice = new ComplexInvoice(21439, new Date());
+
+        BadInvoicePrinter_I badInvoicePrinter = new BadInvoicePrinter();
+        badInvoicePrinter.print(invoice);
+        badInvoicePrinter.printComplexInvoice(complexInvoice);
+        badInvoicePrinter.someOtherPrintMethod(invoice); //why should I be able to do this?
+
+        BadInvoicePrinter_I someOtherBadInvoicePrinter = new SomeOtherBadInvoicePrinter();
+        someOtherBadInvoicePrinter.print(invoice); //why should I be able to do this?
+        someOtherBadInvoicePrinter.printComplexInvoice(complexInvoice); //why should I be able to do this?
+        someOtherBadInvoicePrinter.someOtherPrintMethod(invoice);
     }
 
-    private static void testGoodL() {
-        Shape rectangle = new GoodRectangle(29, 40);
-        Shape square = new GoodSquare(20);
-        GoodGraphicsService goodGraphicsService = new GoodGraphicsService();
+    private static void testGoodI() {
+        Invoice invoice = new Invoice(423);
+        ComplexInvoice complexInvoice = new ComplexInvoice(242, new Date());
 
-        goodGraphicsService.checkForArea(rectangle);
-        goodGraphicsService.checkForArea(square);
+        GoodInvoicePrinter_I goodInvoicePrinter = new GoodInvoicePrinter();
+        goodInvoicePrinter.print(invoice);
+        goodInvoicePrinter.printComplexInvoice(complexInvoice);
+        // goodInvoicePrinter.someOtherPrintMethod(invoice); // unavailable! great.
 
+
+        SomeOtherGoodInvoicePrinter_I someOtherGoodInvoicePrinter = new SomeOtherGoodInvoicePrinter();
+        // other 2 methods are unavailable
+        someOtherGoodInvoicePrinter.someOtherPrintMethod(invoice);
     }
 
 }
---
+++ src/good/SomeOtherGoodInvoicePrinter_I.java
@@ -0,0 +1,8 @@
+package good;
+
+import invoice.ComplexInvoice;
+import invoice.Invoice;
+
+public interface SomeOtherGoodInvoicePrinter_I {
+    void someOtherPrintMethod(Invoice invoice);
+}
