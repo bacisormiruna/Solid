fcee59af633cde023d0a12dddad13cc6ee24bca4
bacisormiruna <mirunabacisor06@gmail.com>
2024-10-15 19:07:11 +0300

    Added class for 'O' principle in solid

--- src/good/AnotherGoodClient.java
+++ src/good/AnotherGoodClient.java
@@ -2,6 +2,6 @@ package good;
 
 public class AnotherGoodClient implements Client_I{
     public void doSomething() {
-        System.out.println("I am doing something");
+        System.out.println("I am doing something else");
     }
 }
